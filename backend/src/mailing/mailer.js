5ca40f670b0d8421786b4e89a2ddd07584a107f3
Teo Ryan <ryan@dyn192102.cc.lehigh.edu>
2026-04-22 13:13:28 -0400

    implemented mailhog updated readme

--- backend/src/mailing/mailer.js
+++ backend/src/mailing/mailer.js
@@ -1,6 +1,10 @@
 import nodemailer from "nodemailer";
 
-export const transporter = nodemailer.createTransport({
+const isProd = process.env.SMTP_USER && process.env.SMTP_PASS;
+
+export const transporter = isProd ? 
+// production mail service
+nodemailer.createTransport({
   host: "smtp.hostinger.com",
   port: 465,
   secure: true,
@@ -8,4 +12,10 @@ export const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+}) : 
+// mailhog for dev
+nodemailer.createTransport({
+  host: "localhost",
+  port: 1025,
+  secure: false,
 });
